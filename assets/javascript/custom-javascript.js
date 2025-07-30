b25c284889eb5122f16ac15ab9a4898374541905
okkybura <okkyburapixel@hotmail.com>
2025-07-30 18:14:19 +0700

    first commit

---
+++ assets/javascript/custom-javascript.js
@@ -0,0 +1,192 @@
+﻿
+/* -------------------- Browser Zoom Setting -------------------- */
+
+window.addEventListener('wheel', function (e) {
+    if (e.ctrlKey) {
+        e.preventDefault();
+    }
+},
+{
+    passive: false
+});
+
+//window.addEventListener('keydown', function (e) {
+//    if (e.ctrlKey) {
+//        if (['+', '-', '=', '0', 'numpadAdd', 'numpadSubtract', 'numpad0'].includes(e.key) ||
+//            e.keyCode === 187 || e.keyCode === 189 || e.keyCode === 48) {
+//            e.preventDefault();
+//        }
+//    }
+//});
+
+/* -------------------- Cursor Setting -------------------- */
+
+const cursorDefault = new MouseFollower({
+    skewing: 1,
+    speed: 0.5,
+    visible: false,
+});
+
+const targets = document.querySelectorAll('[data-cursor]');
+
+targets.forEach(target => {
+    target.addEventListener('mouseenter', () => {
+        cursorDefault.show();
+    });
+
+    target.addEventListener('mouseleave', () => {
+        cursorDefault.hide();
+    });
+});
+
+/* -------------------- GSAP Setting -------------------- */
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.registerPlugin(Observer, ScrollTrigger, ScrollSmoother, ScrollToPlugin, SplitText)
+});
+
+// Scroll Smoother
+
+ScrollSmoother.create({
+    smooth: 1,
+    effects: true,
+    smoothTouch: 0.5,
+});
+
+document.querySelectorAll(".text-mask").forEach((el) => {
+    const splitText = SplitText.create(el, {
+        type: "chars,words,lines",
+        linesClass: "line",
+        autoSplit: true,
+        mask: "lines",
+    });
+});
+
+// Split Text
+
+document.querySelectorAll(".text-mask-reveal").forEach((el) => {
+    const start = el.dataset.start || "top 75%";
+    const splitText = SplitText.create(el, {
+        type: "words,lines",
+        linesClass: "line",
+        autoSplit: true,
+        mask: "lines",
+        onSplit: (self) => {
+            gsap.set(self.lines, { yPercent: 100 });
+            ScrollTrigger.create({
+                trigger: el,
+                start: start,
+                markers: false,
+                onEnter: () => {
+                    gsap.to(self.lines, {
+                        duration: 1.5,
+                        yPercent: 0,
+                        stagger: 0.065,
+                        ease: "expo.out",
+                    });
+                },
+            });
+        }
+    });
+});
+
+/* -------------------- Flickty Setting -------------------- */
+
+document.querySelectorAll('.carousel').forEach(function (carouselElem) {
+    var flkty = new Flickity(carouselElem, {
+        imagesLoaded: true,
+        percentPosition: false,
+        freeScroll: true,
+        contain: true,
+        prevNextButtons: false,
+        pageDots: false
+    });
+
+    var imgs = carouselElem.querySelectorAll('.carousel-cell img');
+    var docStyle = document.documentElement.style;
+    var transformProp = typeof docStyle.transform === 'string' ? 'transform' : 'WebkitTransform';
+
+    flkty.on('scroll', function () {
+        flkty.slides.forEach(function (slide, i) {
+            var img = imgs[i];
+            if (img) {
+                var x = (slide.target + flkty.x) * -1 / 5;
+                img.style[transformProp] = 'translateX(' + x + 'px)';
+            }
+        });
+    });
+});
+
+/* -------------------- Custom Split Text Setting -------------------- */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Splitting Text - Title
+
+    document.querySelectorAll('.text-title').forEach(btn => {
+
+        const textSpan = btn.querySelector('.text');
+
+        if (textSpan) {
+            const text = textSpan.textContent.trim();
+            let charSpans = '';
+
+            text.split('').forEach(char => {
+                charSpans += `<span>${char}</span>`;
+            });
+
+            textSpan.innerHTML = `
+                        <span>${charSpans}</span>
+                    `;
+        }
+    });
+
+    // Splitting Text - Link - Navbar
+
+    document.querySelectorAll('.nav-link').forEach(btn => {
+
+        const textSpan = btn.querySelector('.text');
+
+        if (textSpan) {
+            const text = textSpan.textContent.trim();
+            let charSpans = '';
+
+            text.split('').forEach(char => {
+                charSpans += `<span>${char}</span>`;
+            });
+
+            textSpan.innerHTML = `
+                        <span>${charSpans}</span>
+                        <span>${charSpans}</span>
+                    `;
+        }
+    });
+
+    // Splitting Text - Button
+
+    document.querySelectorAll('.btn').forEach(btn => {
+
+        const textSpan = btn.querySelector('.text');
+
+        if (textSpan) {
+            const text = textSpan.textContent.trim();
+            let charSpans = '';
+
+            text.split('').forEach(char => {
+                charSpans += `<span>${char}</span>`;
+            });
+
+            textSpan.innerHTML = `
+                        <span>${charSpans}</span>
+                        <span>${charSpans}</span>
+                    `;
+        }
+    });
+});
+
+/* -------------------- Navbar Setting -------------------- */
+
+$(window).scroll(function () {
+    $(".navbar").toggleClass("scroll", $(this).scrollTop() > 1)
+    $("#scroll-top").toggleClass("scroll", $(this).scrollTop() > 1)
+});
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  // content: [
+  //   "./src/**/*.{html,js,ts,jsx,tsx,mdx}",
+  // ],
+  theme: {
+    screens: {
+      "2xl": { max: "1980px" },
+      "xl": { max: "1680px" },
+      "lg": { max: "1440px" },
+      "md": { max: "1024px" },
+      "sm": { max: "769px" },
+      "xs": { max: "480px" },
+      "xxs": { max: "379px" }
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          '2xl': '1140px',
+          xl: '1140px',
+          lg: '960px',
+          md: '720px',
+          sm: '540px',
+          xs: '100%',
+          xxs: '100%',
+        },
+      },
+      colors: {
+        primary: '#4444DD',
+      },
+      height: {
+        screen: "100vh",
+      },
+      fontFamily: {
+        archivo: ["Archivo", "sans-serif"],
+        bebasNeue: ["BebasNeue", "sans-serif"],
+        cabinetGrotesk: ["CabinetGrotesk", "sans-serif"],
+        clashDisplay: ["ClashDisplay", "sans-serif"],
+        familjenGrotesk: ["FamiljenGrotesk", "sans-serif"],
+        switzer: ["Switzer", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
